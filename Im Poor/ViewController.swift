9c91b52a5935ffa91af426ad235b5e6439808bba
TatarProger <super.pozitiv.2017@mail.ru>
2023-10-04 23:51:03 +0300

    Initial Commit

---
+++ Im Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Im Poor
+//
+//  Created by Rishat Zakirov on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
